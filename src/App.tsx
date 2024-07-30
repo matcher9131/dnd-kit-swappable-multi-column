a12a81185692183baaac6a903b450834c2800550
matcher9131 <matcher9131cd@gmail.com>
2024-07-31 08:36:50 +0900

    Set bg color of Item from outer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Container from "./components/container/Container";
 import Item from "./components/item/Item";
 import { containerChildrenState } from "./models/containerChildren";
 
+const getItemBgColor = (labelText: string): string => {
+    return labelText.startsWith("A") ? "bg-red-300" : labelText.startsWith("B") ? "bg-green-300" : "bg-blue-300";
+};
+
 const App = (): JSX.Element => {
     const columns = useRecoilValue(containerChildrenState);
 
@@ -13,7 +17,7 @@ const App = (): JSX.Element => {
                 {columns.map((column) => (
                     <Column key={column.header} header={column.header}>
                         {column.items.map((item) => (
-                            <Item key={item} labelText={item} />
+                            <Item key={item} labelText={item} className={getItemBgColor(item)} />
                         ))}
                     </Column>
                 ))}
--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,13 +1,10 @@
-const getBgColor = (labelText: string): string => {
-    return labelText.startsWith("A") ? "bg-red-300" : labelText.startsWith("B") ? "bg-green-300" : "bg-blue-300";
-};
-
 type ItemProps = {
     readonly labelText: string;
+    readonly className: string;
 };
 
-const Item = ({ labelText }: ItemProps): JSX.Element => {
-    return <div className={`w-full flex items-center p-2 ${getBgColor(labelText)}`}>{labelText}</div>;
+const Item = ({ labelText, className }: ItemProps): JSX.Element => {
+    return <div className={`w-full flex items-center p-2 ${className}`}>{labelText}</div>;
 };
 
 export default Item;
